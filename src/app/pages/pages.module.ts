529228c590f0184b126434d25a6d6c1f443fb5a3
Camilo Muñoz <camilo.mh2014@gmail.com>
2021-03-16 21:45:37 -0300

    Graficos

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -3,6 +3,9 @@ import { CommonModule } from '@angular/common';
 
 //Modulo
 import {RouterModule} from '@angular/router';
+import {FormsModule} from '@angular/forms';
+import { ComponentsModule } from '../components/components.module';
+import { SharedModule } from '../shared/shared.module';
 
 
 //Componentes
@@ -10,7 +13,6 @@ import { PagesComponent } from './pages.component';
 import { Grafica1Component } from './grafica1/grafica1.component';
 import { ProgressComponent } from './progress/progress.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { SharedModule } from '../shared/shared.module';
 
 
 
@@ -25,9 +27,12 @@ import { SharedModule } from '../shared/shared.module';
     PagesComponent
   ],
   imports: [
+    FormsModule,
     CommonModule,
     SharedModule,
-    RouterModule
+    RouterModule,
+    ComponentsModule,
+  
   ],
   exports:[
     DashboardComponent,
